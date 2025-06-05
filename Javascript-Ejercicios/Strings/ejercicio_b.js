089f04f172474d4affefa6add65b1fa8c809afc1
Bautista <bautistamarinelliscaglia@gmail.com>
2025-06-05 11:45:16 -0300

    Ejercicio JS tema 1 y 2 terminado

---
+++ Javascript-Ejercicios/Strings/ejercicio_b.js
@@ -0,0 +1,5 @@
+var texto = "almenosdiezcaracteres";
+
+var nuevoString = texto.substring(0, 5);
+
+console.log(nuevoString); 
---
+++ Javascript-Ejercicios/Strings/ejercicio_e.js
@@ -0,0 +1,5 @@
+var texto = "almenos diezcaracteres";
+
+var primer_espacio = texto.indexOf(" ");
+
+console.log(primer_espacio);
